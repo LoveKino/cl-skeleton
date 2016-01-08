50865129035660b679f78149e257c3e4bdf60663
ddchen <842914439@qq.com>
2016-01-08 16:44:57 +0800

    opts

--- skeletons/babel-node-lib/test/src/base.js
+++ skeletons/babel-node-lib/test/src/base.js
@@ -1,6 +1,6 @@
-import index from '../../index';
+// import 'babel-polyfill'; // if you need to some feature like async await, open this notation.
 
-// require('babel-polyfill'); // if you need to some feature like async await, open this notation.
+import index from '../../index';
 
 describe('base', () => {
     it('base', () => {});
